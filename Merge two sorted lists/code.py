15007abb0328c9e571da00d718e7c46d676ef53f
elhamzeinodini <elhammth@gmail.com>
2022-05-16 16:25:23 +0430

    fixed some bug

--- Merge two sorted lists/code.py
+++ Merge two sorted lists/code.py
@@ -1,8 +1,10 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        
+        
 class Solution:
     def mergeLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         dummyNode = ListNode()

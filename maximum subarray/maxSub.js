995157adf1dad9ec3cb55deac3b6f7c88266f582
elhamzeinodini <elhammth@gmail.com>
2022-05-16 15:42:31 +0430

    optimized the code in js

--- maximum subarray/maxSub.js
+++ maximum subarray/maxSub.js
@@ -1,9 +1,15 @@
-function maxSum(nums) {
+const solution = (array) => {
     let currentSum = 0
-    let maxSub = nums[0]
-    nums.forEach((i, a) => {
-        currentSum = Math.max(i, currentSum + i)
+    let maxSub = array[0]
+    array.forEach((item) => {
+        if (currentSum < 0) {
+            currentSum = 0
+        }
+        currentSum += item
         maxSub = Math.max(maxSub, currentSum)
     })
     return maxSub
 }
+
+
+console.log(solution([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
